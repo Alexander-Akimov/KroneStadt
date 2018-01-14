54a342c231de2f593a892dae3354cfefb686a046
Alexander-Akimov <lex@Akimovs-iMac.local>
2018-01-14 21:10:26 +0600

    using simple basic adapter, added layout

--- Projs/CoderSwag/app/src/main/java/com/akimov/coderswag/controller/MainActivity.kt
+++ Projs/CoderSwag/app/src/main/java/com/akimov/coderswag/controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.akimov.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.akimov.coderswag.R
+import com.akimov.coderswag.Services.DataService
+import com.akimov.coderswag.model.Category
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        cartegoryListView.adapter = adapter
     }
 }
--- Projs/CoderSwag/app/src/main/java/com/akimov/coderswag/model/Category.kt
+++ Projs/CoderSwag/app/src/main/java/com/akimov/coderswag/model/Category.kt
@@ -3,4 +3,8 @@ package com.akimov.coderswag.model
 /**
  * Created by lex on 1/14/18.
  */
-class Category (val title: String, val image: String)
+class Category (val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
