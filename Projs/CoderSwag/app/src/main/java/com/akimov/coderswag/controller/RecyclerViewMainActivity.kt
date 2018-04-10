31d7f63721dccfffe1a6796d6907b6bf5a38a618
Alexander-Akimov <lex@Akimovs-iMac.local>
2018-04-10 14:44:53 +0600

    prepare for recycler view adapter

---
+++ Projs/CoderSwag/app/src/main/java/com/akimov/coderswag/controller/RecyclerViewMainActivity.kt
@@ -0,0 +1,13 @@
+package com.akimov.coderswag.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.akimov.coderswag.R
+
+class RecyclerViewMainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_recycler_view_main)
+  }
+}
